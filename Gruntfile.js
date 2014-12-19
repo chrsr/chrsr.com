226fb71453b5b966e8ef4c8f3bee4803b93204e6
Chris Roberts <chris.roberts@wotifgroup.com>
2014-12-19 14:55:39 +1000

    Complete lifecycle with grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
 
+    grunt.initConfig({
         src: {
             base: 'src/',
             styles: '<%= src.base %>less/',
@@ -11,16 +13,16 @@ module.exports = function(grunt) {
 
         dist: {
             base: 'dist/',
-            styles: '<%= dist.base %>css/',
+            css: '<%= dist.base %>css/',
             js: '<%= dist.base %>js/'
         },
 
         less: {
-            dev: {
+            src: {
                 options: {
                     paths: ['<%= src.styles %>'],
-                    sourceMap: true,
-                    compress: true
+                    compress: true,
+                    sourceMap: true
                 },
                 files: [{
                     expand: true,
@@ -29,29 +31,30 @@ module.exports = function(grunt) {
                     dest: '<%= src.css %>',
                     ext: '.css'
                 }]
-            },
-            dist: {
-                options: {
-                    paths: ['<%= src.styles %>'],
-                    sourceMap: true,
-                    compress: true
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= src.styles %>',
-                    src: ['**/*.less'],
-                    dest: '<%= dist.css %>',
-                    ext: '.css'
-                }]
             }
         },
 
-        // autoprefixer: {
-        //     options: {},
-        //     no_dest: {
-        //         src: '<%= dist.styles %>'
-        //     }
-        // },
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish')
+            },
+            all: [
+                'Gruntfile.js',
+                'src/**/*.js'
+            ]
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions']
+            },
+            all: {
+                expand: true,
+                flatten: true,
+                src: 'src/css/*.css',
+                dest: 'dist/css/'
+            }
+        },
 
         htmlmin: {
             dist: {
@@ -59,16 +62,65 @@ module.exports = function(grunt) {
                     removeComments: true,
                     collapseWhitespace: true
                 },
-                files: {
-                    'dist/index.html': 'src/index.html'
-                }
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: '**/*.html',
+                    dest: 'dist/'
+                }]
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: 'src/',                   // Src matches are relative to this path
+                    src: '**/*.{png,jpg,gif}',   // Actual patterns to match
+                    dest: 'dist/'                  // Destination path prefix
+                }]
+            }
+        },
+
+        concat: {
+            dist: {
+                src: 'src/js/**/*.js',
+                dest: 'dist/js/scripts.js'
+            }
+        },
+
+        uglify: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist/js',
+                    src: '**/*.js',
+                    dest: 'dist/js'
+                }]
             }
         },
 
         watch: {
             autoprefixer: {
-                files: ['www/***/**/*.css'],
+                files: ['src/***/**/*.css'],
                 tasks: ['autoprefixer']
+            },
+            js: {
+                files: ['src/**/*.js'],
+                tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            styles: {
+                files: ['styles/**/*.less'],
+                tasks: ['autoprefixer', 'less']
+            },
+            options: {
+                livereload: true
             }
         },
 
@@ -84,12 +136,28 @@ module.exports = function(grunt) {
 
     });
 
-    // Load plugins
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    // Tasks
+    grunt.registerTask('default', ['']);
 
-    // Define tasks
-    grunt.registerTask('default', ['less']);
-    grunt.registerTask('build', ['less', 'htmlmin', 'copy']);
+    grunt.registerTask('dev', function () {
+        grunt.task.run([
+            'less',
+            'autoprefixer',
+            'connect',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('build', function () {
+        grunt.task.run([
+            'jshint',
+            'concat',
+            'uglify',
+            'less',
+            'autoprefixer',
+            'imagemin',
+            'htmlmin'
+        ]);
+    });
 
 };
