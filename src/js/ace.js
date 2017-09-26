181235c10cbf585bd9e760d2a408ea9aabaddabe
Chris Roberts <chriroberts@expedia.com>
2017-09-27 09:58:38 +1000

    Clean up

--- src/js/ace.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/tomorrow_night");
-    editor.getSession().setMode("ace/mode/html");
-    editor.setValue(document.documentElement.innerHTML);
-}());
