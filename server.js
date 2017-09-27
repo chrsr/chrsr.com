9aae7aaee45a7759c881c11cdc226d22a300301e
Chris Roberts <chriroberts@expedia.com>
2017-09-27 10:13:44 +1000

    update public directory

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
 const port = process.env.PORT || 8080;
 app.listen(8080, function () {
