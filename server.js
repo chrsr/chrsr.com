ab417c081a2240f4bd6a69928655576342ef0258
Chris Roberts <chriroberts@expedia.com>
2017-09-27 10:19:57 +1000

    minor express syntax changes

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 
 app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 8080));
 
-const port = process.env.PORT || 8080;
-app.listen(8080, function () {
+app.get('/', function(request, response) {
+    response.render('public/index.html');
+});
+
+app.listen(app.get('port'), function() {
     console.log('chrsr.com up and running')
 })
